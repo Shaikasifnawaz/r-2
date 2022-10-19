a0b607ae705b8445e1b875f90b674c4eba89066c
Shaikasifnawaz <98202190+Shaikasifnawaz@users.noreply.github.com>
2022-10-19 13:11:06 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header(){
+    return (
+        <header className="bg-danger py-5">
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+                <h1 className="display-4 fw-bolder">INTRESTING FINDS</h1>
+                <p className="lead fw-normal text-white-50 mb-0">Build you a custom shop with other items</p>
+            </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header;
